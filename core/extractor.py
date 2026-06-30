58ef00aa229f1b25588eb15af470ad4561054bc7
LetC0de <abhishekjiiofficial86@gmail.com>
2026-06-30 18:35:38 +0530

    feat: add extractor module with get_llm function for Mistral AI integration

---
+++ core/extractor.py
@@ -0,0 +1,11 @@
+#Actionableitems , decision , questions 
+
+from langchain_mistralai import ChatMistralAI
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.runnables import RunnablePassthrough, RunnableLambda
+import os 
+
+
+def get_llm():
+    return ChatMistralAI(model = "mistral-small-latest", mistral_api_key = os.getenv("MISTRAL_API_KEY"),temperature=0.2)
